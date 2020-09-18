eabc5c41cefb984336f1bd6347d6db2ba30f3b1f
Kayla <Kayla@DESKTOP-59SFP28>
2020-09-18 10:06:48 -0500

    Add project files.

---
+++ CountVowels/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CountVowels
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txtVowels = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(467, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a paragraph of text. All vowels, upper and";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(540, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "lowercase will be counted. Y is not considered a vowel.";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(44, 137);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(479, 223);
+            this.txtText.TabIndex = 2;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(53, 387);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(165, 67);
+            this.btnCount.TabIndex = 3;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // txtVowels
+            // 
+            this.txtVowels.Location = new System.Drawing.Point(258, 405);
+            this.txtVowels.Name = "txtVowels";
+            this.txtVowels.ReadOnly = true;
+            this.txtVowels.Size = new System.Drawing.Size(117, 31);
+            this.txtVowels.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 405);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "vowels";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 491);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVowels);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Count Vowels";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtVowels;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CountVowels/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CountVowels
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            txtVowels.Text = "";
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+           int vowel = 0;
+
+            string text = txtText.Text.ToLower();
+
+            int len = text.Length;
+
+            for(int i = 0; i< len; i++)
+            {
+
+                if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u')
+                {
+                    vowel++;
+                }
+                
+            }
+
+            txtVowels.Text = Convert.ToString(vowel);
+
+        }
+    }
+}
